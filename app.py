33e63c49e5e994dd3a2cff5e0f2381a46af88cd1
lucasrpinto <lucasrop22@gmail.com>
2025-03-21 17:07:26 -0300

    feat(user): add route to delete user by id

--- app.py
+++ app.py
@@ -80,6 +80,20 @@ def update_user(id_user):
     
     return jsonify({'message': 'Usuário não encontrado'}), 404
 
+@app.route('/user/<int:id_user>', methods=['DELETE'])
+@login_required
+def delete_user(id_user):
+    user = User.query.get(id_user)
+
+    if  id_user == current_user.id:
+        return jsonify({'message': 'Usuário logado, não pode ser excluido!'}), 403
+    if user and id_user:
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify({'message': f'Usuário {id_user} removido com sucesso!'})
+    
+    return jsonify({'message': 'Usuário não encontrado!'}), 404
+
 # Session <- Conexão ativa
 if __name__ == '__main__':
     app.run(debug=True)
