7818f76d1e64ec5328c0284db0b282a1b33f70cf
lucasrpinto <lucasrop22@gmail.com>
2025-03-24 21:30:06 -0300

    refactor: replace SQLite with MySQL using Docker setup

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from database import db
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:admin@127.0.0.1:3306/flask-crud'
 
 login_manager = LoginManager()
 db.init_app(app)
